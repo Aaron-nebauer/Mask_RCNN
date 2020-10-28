decf8eb78cb8362840c556a3e88bc494ac4b2c64
Aaron Nebauer <n9933913@qut.edu.au>
2020-10-28 17:36:27 +1000

    remove print

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -1262,7 +1262,6 @@ def load_image_gt(dataset, config, image_id, augment=False, augmentation=None,
     # and here is to filter them out
     _idx = np.sum(mask, axis=(0, 1)) > 0
     mask = mask[:, :, _idx]
-    print("Idx = ",_idx)
     class_ids = class_ids[_idx]
     # Bounding boxes. Note that some boxes might be all zeros
     # if the corresponding mask got cropped out.
